cefae0bcf28fe264da96d9981bf77e6804c98643
clayton <writeclayton@gmail.com>
2024-02-16 10:48:12 -0600

    tests improved, problem solved

--- js/invalidTransactions.js
+++ js/invalidTransactions.js
@@ -1,64 +1,30 @@
-/**********************************************************************************************************************
- * Columbia Insurance Group Take-home Coding Assessment - Invalid Transactions
- *
- * Thank you for your interest in working with us, and for taking the time to complete this coding assessment! As with
- * most development, there are many possible solutions to this problem (many not "right" or "wrong"), and there may be
- * ambiguities on our part, too. So, we are not necessarily looking for the perfect or most-efficient solution; but
- * rather, deeper insights into your problem-solving approach and coding-style. Because of that, please avoid
- * researching and mimicking ways that other people have solved this type of problem. We are most interested in YOUR
- * own unique solution and style! If you perceive any ambiguities or questions, please feel free to make comments in
- * your work acknowledging them so we can take that into account, and just try your best to work around those.
- *
- * In addition, there is a corresponding unit-test file accompanying this file. In that is a basic sample test that
- * you may run to check your work. While we don't expect you to provide any extra unit tests, please feel free to do
- * so, if your style is TDD. There are no npm scripts provided, so just run: `jest invalidTransactions.test.js`
- *
- * For this assessment, please create your own Node.js project on your local machine and work on it there in whatever
- * IDE you like. There are no required dependencies to the project, unless you want to leverage unit-testing (in which
- * case, you'll need Jest).
- *
- * If you have a GitHub account, we would appreciate if you create a repo and push your work into that as you do it,
- * complete with commit history. When ready to complete the assessment and submit it, we can more easily review your
- * work that way. If you do not have a GitHub account, then please email us back the files you worked on, when done.
- *
- * Good luck, and we hope you can have some fun with this!
- *
- *
- * INSTRUCTIONS:
- * Given an array of transactions (where each element is a single transaction), determine which transaction(s) are
- * invalid and return them in an array (see OUTPUT below for details). A transaction is considered invalid if:
- *  - The amount exceeds $1000, or
- *  - If it occurs within (and including) 60 minutes of another transaction by the same person AND in a different city.
- *
- *
- * INPUT:
- * Each transaction in the input array that the function will process is a string consisting of comma-separated values
- * representing the name, timestamp, amount, and city of the transaction (in that order). The transactions in the array
- * will always be sorted ascending by timestamp.
- *  - Note: The time value is in epoch format (milliseconds since 1970) --e.g. like what Date.now() returns.
- *  - Example input data:
- *      [
- *          "John,1707625218560,300,Chicago",
- *          "John,1707625219542,310,Houston",
- *          "John,1707628819542,205,Houston",
- *          "Alice,1707625258461,195,Denver",
- *          "Bob,1707625259212,1250,Atlanta"
- *      ]
- *
- *
- * OUTPUT:
- * Return an array of those transactions that you deem to be invalid. Any transactions in the resulting array should
- * be in ascending order by time.
- *  - Valid return-data based on the above example input:
- *      [
- *          "John,1707625218560,300,Chicago",  <-- Invalid because within 60 minutes of the $310 Houston transaction
- *          "John,1707625219542,310,Houston",  <-- Invalid because within 60 minutes of the $300 Chicago transaction
- *          "Bob,1707625259212,1250,Atlanta"   <-- Invalid because amount is over the $1,000 threshold
- *      ]
- *  - Explanation:
- *      - Two of John's transactions are within 60 minutes of each other AND from different cities so BOTH are invalid.
- *      - Bob's transaction is over $1000 so it is invalid.
- *********************************************************************************************************************/
+// Submitted by Clayton Roberts
+
+// Thank you for the opportunity to work on this code challenge.
+// I had never used Jest before, so it was interesting to me to try that. Even just creating usable test data and going through to make the test output match the requirements was a pretty big task and a good learning opportunity.
+// Overall, this is an interesting/fun problem.
+// The author makes it seem easy with the first requirement (amount > 1000), but then the second requirement (the user / city / time check) is a big jump in complexity.
+// And then making sure the output is sorted by time, and that that there are no duplicates adds another layer of complexity.
+
+// Notes:
+
+// 1. I'm concerned about the size of the data set possibly being too large for my nested loop and causing a timeout. But that's the approach I took.
+// Normally, I'd lean on sql and return desired results via a query rather than looping through data in JS. Or I'd use a batch job of some kind if timing out is a probability.
+
+// 2. There is some ambiguity in the instructions:
+// If a customer has - say - 3 transactions within 60 minutes and the middle record having a different city. (with the top / most recent coming "1st" in array being called "1st")
+// ...if 2nd record is a different city, then the 2nd and 3rd records are invalid according to instructions.
+// One could argue that the 1st or a subsequent 4th entry might also need to be flagged as invalid if within the same 60 minutes.
+// However, that'd add a lot of complexity, and instructions didn't explicitly call for that. So, I'm assuming that just the 2nd and 3rd records in this example I'm describing should be flagged as invalid.
+
+// 3. I need to be careful of when to add the over-amount records to the return data. I don't want it to step on the toes of my j loop.
+
+// 4. For test creation, I researched the MS timestamps and used a site that converts them to human-readable format. Normally I'd use Moment for this.
+// I then used a spreadsheet to put together the big test data, test 2.
+// I also looked up the.sort to confirm my syntax.
+
+// I'd love to hear any feedback or critique you have. I'm always looking to improve. Thanks again for the opportunity.
+// - Clayton
 
 /**
  * Main Function -- DO YOUR WORK HERE
@@ -68,21 +34,55 @@
 const invalidTransactions = (transactions) => {
   let invalidTxs = [];
 
-  transactions.forEach((transactionString) => {
-    const [name, timestamp, amount, city] = transactionString.split(",");
-    // console.log(name, timestamp, amount, city);
+  // addedTxIndexes array: if the j loop transaction was already added to the invalidTxs array as an i/j pair, then we don't want to add it again once the i loop is on that item which was removed as a j.
+  let addedTxIndexes = [];
 
-    if (isOverMaxAmount(amount)) {
-      invalidTxs = [...invalidTxs, transactionString];
+  // traditional for loop so I can break out easily and not waste compute power. Also, when nesting loops, the i and j loops are more reader-friendly imo.
+  for (let i = 0; i < transactions.length; i++) {
+    const [name, timestamp, amount, city] = transactions[i].split(",");
+    const timestampInt = parseInt(timestamp);
+    const amountInt = parseInt(amount);
+    const oneHourInMs = 3600000;
+    const maxAmount = 1000;
+
+    let txIRemoved = false; // in-loop flag to avoid double write below for when reaching the amount check (in case the item was already flagged in the i/j loop).
+
+    if (!addedTxIndexes.includes(i)) {
+      for (let j = i + 1; j < transactions.length; j++) {
+        const [name2, timestamp2, amount2, city2] = transactions[j].split(",");
+        const timestampInt2 = parseInt(timestamp2);
+
+        if (Math.abs(timestampInt2 - timestampInt) > oneHourInMs) {
+          break; // no need to get into the city check if the timestamps are more than an hour apart. and break is ok (rather than continue) bc input records are in order of time.
+        } else {
+          if (name === name2) {
+            if (Math.abs(timestampInt2 - timestampInt) <= oneHourInMs) {
+              if (city !== city2) {
+                invalidTxs = [...invalidTxs, transactions[i], transactions[j]];
+                addedTxIndexes = [...addedTxIndexes, i, j];
+                txIRemoved = true;
+                break; // break logic would need to change if - as described with ambiguity above - we needed to flag a 1st and/or especially a 4th or subsequent records as invalid as well.
+              }
+            }
+          }
+        }
+      }
+
+      if (txIRemoved === false) {
+        if (amountInt > maxAmount) {
+          invalidTxs = [...invalidTxs, transactions[i]];
+        }
+      }
     }
+  }
+
+  const sortedTransactions = invalidTxs.sort((a, b) => {
+    const timestampA = parseInt(a.split(",")[1]);
+    const timestampB = parseInt(b.split(",")[1]);
+    return timestampA - timestampB;
   });
-  // console.log("invalidTxs >>>>>>>> ", invalidTxs);
 
-  return invalidTxs;
-};
-
-const isOverMaxAmount = (amount) => {
-  return parseInt(amount) > 1000;
+  return sortedTransactions;
 };
 
 module.exports = invalidTransactions; // Do NOT modify or remove this
--- js/invalidTransactions.test.js
+++ js/invalidTransactions.test.js
@@ -1,7 +1,8 @@
 const invalidTransactions = require("./invalidTransactions");
 
 // Sample/basic unit test
-test("1. Ability to flag invalid transactions based on: 1. dollar amount", () => {
+
+test("1. Ability to flag invalid transactions based on: 2. timestamp/city and amount", () => {
   expect(
     invalidTransactions([
       "John,1707625218560,300,Chicago",
@@ -10,23 +11,96 @@ test("1. Ability to flag invalid transactions based on: 1. dollar amount", () =>
       "Alice,1707625258461,195,Denver",
       "Bob,1707625259212,1250,Atlanta",
     ])
-  ).toStrictEqual(["Bob,1707625259212,1250,Atlanta"]);
+  ).toStrictEqual([
+    "John,1707625218560,300,Chicago",
+    "John,1707625219542,310,Houston",
+    "Bob,1707625259212,1250,Atlanta",
+  ]);
 });
 
-// test("2. Ability to flag invalid transactions based on: 2. timestamp", () => {
-//   expect(
-//     invalidTransactions([
-//       "John,1707625218560,300,Chicago",
-//       "John,1707625219542,310,Houston",
-//       "John,1707628819542,205,Houston",
-//       "Alice,1707625258461,195,Denver",
-//       "Bob,1707625259212,1250,Atlanta",
-//     ])
-//   ).toStrictEqual([
-//     "John,1707625218560,300,Chicago",
-//     "John,1707625219542,310,Houston",
-//   ]);
-// });
+test("2. BIG ONE -  Ability to flag invalid transactions based on: timestamp and city for same person and amount over 1k", () => {
+  expect(
+    invalidTransactions([
+      "John,1708070400000,300,Chicago",
+      "Alice,1708070880000,195,Denver",
+      "Bob,1708071240000,1250,Atlanta",
+      "John,1708137300000,205,Houston",
+      "Alice,1708137540000,400,Chicago",
+      "Bob,1708138080000,800,Atlanta",
+      "John,1708138380000,310,Chicago",
+      "Alice,1708138500000,500,Denver",
+      "Bob,1708138980000,1200,Atlanta",
+      "John,1708139460000,410,Houston",
+      "Alice,1708140000000,600,Denver",
+      "Bob,1708140300000,1300,Atlanta",
+      "John,1708140600000,420,Houston",
+      "Alice,1708140900000,700,Denver",
+      "Bob,1708141200000,1400,Atlanta",
+      "John,1708141500000,430,Houston",
+      "Alice,1708141800000,800,Denver",
+      "Bob,1708142100000,1500,Atlanta",
+      "John,1708142400000,440,Houston",
+      "Alice,1708142700000,900,Denver",
+      "Bob,1708143000000,1600,Atlanta",
+      "John,1708143300000,450,Houston",
+      "Alice,1708143600000,1000,Denver",
+      "Bob,1708143900000,1700,Atlanta",
+      "Charlie,1708144200000,300,Chicago",
+      "David,1708144500000,500,New York",
+      "Eve,1708144800000,700,Los Angeles",
+      "Charlie,1708145100000,800,Chicago",
+      "David,1708145400000,1000,New York",
+      "Eve,1708145700000,1200,Los Angeles",
+      "Charlie,1708146000000,1300,Chicago",
+      "David,1708146300000,1500,New York",
+      "Eve,1708146600000,1700,Los Angeles",
+    ])
+  ).toStrictEqual([
+    "Bob,1708071240000,1250,Atlanta",
+    "John,1708137300000,205,Houston",
+    "Alice,1708137540000,400,Chicago",
+    "John,1708138380000,310,Chicago",
+    "Alice,1708138500000,500,Denver",
+    "Bob,1708138980000,1200,Atlanta",
+    // "John,1708139460000,410,Houston", // this -and others commented below- not included as invalid. See note in my comments on ambiguity.
+    // "Alice,1708140000000,600,Denver",
+    "Bob,1708140300000,1300,Atlanta",
+    // "John,1708140600000,420,Houston",
+    // "Alice,1708140900000,700,Denver",
+    "Bob,1708141200000,1400,Atlanta",
+    // "John,1708141500000,430,Houston",
+    "Bob,1708142100000,1500,Atlanta",
+    "Bob,1708143000000,1600,Atlanta",
+    "Bob,1708143900000,1700,Atlanta",
+    "Eve,1708145700000,1200,Los Angeles",
+    "Charlie,1708146000000,1300,Chicago",
+    "David,1708146300000,1500,New York",
+    "Eve,1708146600000,1700,Los Angeles",
+  ]);
+});
+
+test("3. one more just in case", () => {
+  expect(
+    invalidTransactions([
+      "John,1708070400000,300,Chicago",
+      "Alice,1708070880000,195,Denver",
+      "Bob,1708071240000,1250,Atlanta",
+      "John,1708137300000,205,Houston",
+      "Alice,1708137540000,400,Chicago",
+      "Bob,1708138080000,800,Atlanta",
+      "John,1708138380000,310,Chicago",
+      "Alice,1708138500000,500,Denver",
+      "Bob,1708138980000,1200,Atlanta",
+    ])
+  ).toStrictEqual([
+    "Bob,1708071240000,1250,Atlanta",
+    "John,1708137300000,205,Houston",
+    "Alice,1708137540000,400,Chicago",
+    "John,1708138380000,310,Chicago",
+    "Alice,1708138500000,500,Denver",
+    "Bob,1708138980000,1200,Atlanta",
+  ]);
+});
 
 // PLEASE PROVIDE ANY ADDITIONAL UNIT TESTS AS YOU DEEM APPROPRIATE!
 // Note: You may use any Jest-compatible methods and patterns, as you wish.
